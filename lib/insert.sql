7499493bcdb8c9b742d12df9c5bfac135cb75d4a
Joyce Njogu <joycenjogu127@gmail.com>
2025-11-28 20:35:30 +0300

    completed sql lab

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES
+('Mr. Chocolate', 10, 'M', 'Brown', 'Calm', 1),
+('Rowdy', 5, 'M', 'Black', 'Aggressive', 1),
+('Tabitha', 7, 'F', 'White', 'Friendly', 1),
+('Sergeant Brown', 12, 'M', 'Brown', 'Serious', 1),
+('Melissa', 6, 'F', 'Golden', 'Gentle', 1),
+('Grinch', 8, 'M', 'Green', 'Grumpy', 0),
+('Wendy', 4, 'F', 'Gray', 'Playful', 1),
+(NULL, 3, 'F', 'Black', 'Shy', 1);
